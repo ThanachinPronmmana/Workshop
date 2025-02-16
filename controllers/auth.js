0f699266ef98313d72ad72f152671a34d48662ac
Thanachin <143328453+ThanachinPronmmana@users.noreply.github.com>
2025-02-16 22:12:49 +0700

    register hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const prisma = require('../config/prisma')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 exports.register = async(req,res)=>{
     try{
         const {email ,password} = req.body
@@ -6,8 +9,33 @@ exports.register = async(req,res)=>{
                 massage:"Email is required!"
             })
         }
-        console.log(email,password)
-        res.send("hello")
+        if(!password){
+            return res.status(400).json({
+                massage:"Password is required!"
+            })
+        }
+        const user = await prisma.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+        if(user){
+            return res.status(400).json({
+                massage:"Email already exits"
+            })
+        }
+        const hashPassword = await bcrypt.hash(password,10)
+
+        await prisma.user.create({
+            data:{
+                email:email,
+                password:hashPassword
+            }
+        })
+    
+
+  
+        res.send("Register Success")
     }catch(err){
         console.log(err)
         res.status(500).json({
